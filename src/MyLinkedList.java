0ca22400823e8d6b4694897018edb35b2dd661e7
Aikusya <butterflyy1211@gmail.com>
2023-07-18 14:14:48 +0600

    lastIndexOf method of MyLinkedList

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -129,7 +129,16 @@ public class MyLinkedList<T> implements MyList<T> {
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        MyNode current = tail;
+        int index = size - 1;
+        while (current != null) {
+            if (current.item.equals(o)) {
+                return index;
+            }
+            current = current.previous;
+            index--;
+        }
+        return -1;
     }
 
     private void checkIndex(int index){
